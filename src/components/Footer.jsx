95e758939d6c569e12c319fc10f389b7979d0023
rohmanalka <rohmanalka06@gmail.com>
2025-10-19 20:05:15 +0700

    feat: add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <main className="mx-[100px]">
+        <div className="mt-32 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center">
+            <h1 className="text-2xl font-bold">
+                <img src="src\assets\images\Logo.png" className="w-[150px] h-[30px]" />
+            </h1>
+            <div className="flex gap-7">
+                <a href="#beranda" className="cursor-target">Beranda</a>
+            </div>
+            <div className="flex items-center gap-3">
+                <a href="https://github.com/rohmanalka" target="_blank">
+                    <i className="ri-github-fill ri-2x cursor-target"></i>
+                    @2025 MyInternet
+                </a>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Footer
