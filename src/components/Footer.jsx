c85bb38e3050a097875ee38eb29cf12265f1f858
rohmanalka <rohmanalka06@gmail.com>
2025-10-21 00:38:06 +0700

    style: styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,10 @@
 const Footer = () => {
   return (
     <main>
-        <div className="mt-32 py-4 px-10 flex md:flex-row flex-col gap-6 md:gap-0 justify-between shadow-inner items-center">
+        <div className="mt-32 py-4 px-25 flex md:flex-row flex-col gap-6 md:gap-0 justify-between shadow-inner items-center">
             <h1 className="text-2xl font-semibold">
                 <img src="/src/assets/images/logo.png" className="h-[40px]"/>
             </h1>
-            <div className="flex gap-7">
-                <a href="#beranda" className="cursor-target font-semibold">Beranda</a>
-            </div>
             <div className="flex items-center gap-3">
                 <a href="https://github.com/rohmanalka" target="_blank">
                     <i className="ri-github-fill ri-2x cursor-target"></i>
